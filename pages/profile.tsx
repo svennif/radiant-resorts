c561aefe8a5d74ebaef79811bd389e97a0c7de2b
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-08-23 13:21:41 +0200

    Added Prisma and other basic stuff

---
+++ pages/profile.tsx
@@ -0,0 +1,21 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { prisma } from "@/lib/prisma";
+
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+export default function Page({ user }: Props) {
+    return <main>Hello {user?.name}</main>;
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const user = await prisma.user.findFirst({
+        where: {
+            email: "test@test.com",
+        },
+    });
+    return {
+        props: {
+            user,
+        },
+    };
+};
