23a032d432c0744ba953c67f27eac4e544d5d4b0
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-09-21 02:45:20 +0200

    Updated stuff

--- app/api/hotels/route.ts
+++ app/api/hotels/route.ts
@@ -1,12 +1,24 @@
 import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-    const hotels = await prisma.hotel.findMany();
+export async function GET(req: NextRequest) {
+    const skip = req.nextUrl.searchParams.get("skip");
+    const take = req.nextUrl.searchParams.get("take");
+
+    const hotels = await prisma.hotel.findMany({
+        skip: skip ? parseInt(skip, 10) : undefined,
+        take: take ? parseInt(take, 10) : undefined,
+    });
     return NextResponse.json(hotels);
 }
 
 export async function POST(req: Request) {
+    const session = await getServerSession();
+    if (!session) {
+        return new NextResponse(null, { status: 401 });
+    }
+
     const json = await req.json();
 
     const created = await prisma.hotel.create({
--- app/hotels/page.tsx
+++ app/hotels/page.tsx
@@ -1,3 +1,19 @@
-export default function Hotels() {
-    return <div>Hotels</div>;
+import { prisma } from "@/lib/prisma";
+
+async function getHotels() {
+    const res = await prisma.hotel.findMany();
+    console.log(res);
+
+    if (!res) {
+        throw new Error("No hotels found.");
+    }
+
+    return 
+}
+export default async function Hotels() {
+    return (
+        <div>
+            <h1>Hotels</h1>
+        </div>
+    );
 }
