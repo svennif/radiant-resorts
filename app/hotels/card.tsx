e5d9c0b095b2fba0e50ec9b8cfa4acc30fc69e06
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-11-12 00:31:39 +0100

    added description

--- app/hotels/card.tsx
+++ app/hotels/card.tsx
@@ -1,11 +1,12 @@
 const Card = ({ hotel }: { hotel: { name: string; address: string; price: number, rating: number } }) => {
-    const { name, address, price, rating } = hotel;
+    const { name, address, price, rating, description } = hotel;
     return (
         <li className="card">
             <div className="card-image-wrapper"></div>
             <div className="card-info">
                 <h3 className="card-title">{name}</h3>
                 <p className="card-address">{address}</p>
+                <p className="card-address">{description}</p>
                 <p className="card-address">Price: {price}</p>
                 <p className="card-address">Rating {rating}</p>
             </div>
