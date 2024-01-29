3ebd011d98a13177e706f7e9b6193f19ade31ef9
Sven Ingar Frantzen <sven.ingar@gmail.com>
2024-01-29 16:31:03 +0100

    I try to do stuff

--- app/hotels/[id]/page.tsx
+++ app/hotels/[id]/page.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import Image from 'next/image'
+'use client'
 
-export default function Page({ hotel }: { id: number; name: string; address: string; price: number; rating: number; description: string; image: string; imageCreds: string }) {
-	const { id, name, address, price, rating, description, image, imageCreds } = hotel
-	const imagePath = `/images/${id}.webp`
-	return (
-		<div>
-			<h1>{name}</h1>
-			<p>{address}</p>
-			<p>{description}</p>
-			<p>Price: {price} €</p>
-			<p>Rating {rating}</p>
-			<Image className="card-image" src={imagePath} alt={imageCreds} />
-		</div>
-	)
+import { useState, useEffect } from 'react'
+import Card from './card'
+import { log } from 'console'
+
+interface Hotel {
+	id: number
+	name: string
+	address: string
+	price: number
+	rating: number
+	description: string
+	image: string
+	imageCreds: string
 }
+
+function Hotels() {
+	const [hotels, setHotels] = useState<Hotel[]>([])
+
+	useEffect(() => {
+		async function fetchHotels() {
+			const res = await fetch('/api/hotels')
+			const data = await res.json()
+      console.log(data.id);
+      
+			setHotels(data)
+		}
+
+		fetchHotels()
+	}, [])
+
+
+}
+
+export default Hotels
